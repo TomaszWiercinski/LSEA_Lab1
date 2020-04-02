62f3f2c4b520fda68f596e83527896f4b7c784a7
TomaszWiercinski <tomaszwiercinski26@gmail.com>
2020-04-02 19:14:53 +0200

    Node implements Comparable; compares lexicographically on title

--- src/main/java/pl/gda/pg/eti/lsea/lab/Node.java
+++ src/main/java/pl/gda/pg/eti/lsea/lab/Node.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Abstract Node class representing every element within the file structure.
  * @author Tomasz Wierciński
  */
-public abstract class Node implements Cloneable {
+public abstract class Node implements Cloneable, Comparable {
     
     protected String title;  // name of the node
     protected Node parent = null;
@@ -41,6 +41,32 @@ public abstract class Node implements Cloneable {
     @Override
     public String toString() {
         return title;
+    }
+    
+    /**
+     * Casts Object to {@link Node} and compares using {@link Node#compareTo(pl.gda.pg.eti.lsea.lab.Node)}.
+     * @param o an Object to compare with
+     * @return 
+     * {@code null} if the Object argument cannot be cast to {@link Node};
+     * the value 0 if the argument Node's title is equal to this Node's title; 
+     * a value less than 0 if this Node's title is lexicographically less than the argument Node's title; 
+     * a value greater than 0 if this Node's title is lexicographically greater than the argument Node's title;.
+     */
+    @Override
+    public int compareTo(Object o) {
+        return this.compareTo((Node) o);
+    }
+    /**
+     * Compares {@link Node} titles lexicographically.
+     * @param anotherNode
+     * A {@link Node} to compare with.
+     * @return 
+     * The value 0 if the argument Node's title is equal to this Node's title; 
+     * a value less than 0 if this Node's title is lexicographically less than the argument Node's title; 
+     * and a value greater than 0 if this Node's title is lexicographically greater than the argument Node's title.
+     */
+    public int compareTo(Node anotherNode) {
+        return title.compareTo(anotherNode.getTitle());
     }
     
     @Override
