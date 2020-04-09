5488f77e898dbf9591649e8bc8045eedbfe50d85
TomaszWiercinski <tomaszwiercinski26@gmail.com>
2020-04-09 12:12:40 +0200

    changed DateComparator to use last edit dates

--- src/main/java/pl/gda/pg/eti/lsea/lab/DateComparator.java
+++ src/main/java/pl/gda/pg/eti/lsea/lab/DateComparator.java
@@ -3,14 +3,14 @@ package pl.gda.pg.eti.lsea.lab;
 import java.util.Comparator;
 
 /**
- * Comparator for comparing Nodes based on creation dates. Orders nodes chronologically.
+ * Comparator for comparing Nodes based on last edit dates. Orders nodes chronologically.
  * @author Tomasz Wierciński
  */
 public class DateComparator implements Comparator<Node> {
 
     @Override
     public int compare(Node o1, Node o2) {
-        return o1.getDateCreated().compareTo(o2.getDateCreated());
+        return o1.getDateEdited().compareTo(o2.getDateEdited());
     }
     
 }
